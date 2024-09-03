3538c4fd6ddc1245a546195263aa3375634ebca3
Cold-chick <zaitkulov4@gmail.com>
2024-09-03 20:05:04 +0500

    first

---
+++ resources/views/index.blade.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="/css/style.css">
+    <title>PoliteRussians</title>
+</head>
+
+<body>
+    <header>
+        <nav>
+            <ul>
+                <li><a href="">Каталог</a></li>
+                <li><a href="">Коллекции</a></li>
+                <li><a href="">Новинки</a></li>
+                <li><a href="">Lookbook</a></li>
+                <li><a href="">Магазины</a></li>
+            </ul>
+        </nav>
+        <a href="/"><img src="./img/logo_black.png" alt=""></a>
+        <div class="user_menu">
+            <img src="/img/search_white.png" alt="Поиск">
+            <p>Поиск</p>
+        </div>
+    </header>
+    <video autoplay loop muted class="bgvideo" id="bgvideo">
+        <source src="/video/index.mp4" type="video/mp4">
+        </source>
+    </video>
+</body>
+
+</html>
